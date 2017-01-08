7f26b1a1b41b19ad06b184652944723174624bea
K1729 <k1729@student.jamk.fi>
2017-01-08 02:26:21 +0200

    retry

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pääsivu</title>
+        <meta name="description" content="My home page">
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" type="text/css" href="main/css/common.css">
+    </head>
+    <body>
+        <header>
+            <h1>Hello!</h1>
+            <nav>
+                <a href="main/hobbies.html">Harrastukset</a>
+                <a href="main/programs.html">Ohjelmat</a>
+                <a href="projects/">Projektit</a>
+            </nav>
+        </header>
+        
+        <section>
+            <p style="text-align:center; padding:5px;"><img style="border: solid 1px black" src="main/img/mina.jpg" alt="mina"></p>
+            <h2>Hei</h2>
+            <p>Teretulemast sivuilleni. Suoritan tällä hetkellä pientä yllä pitoa. Koitan tehdä sivusta helpommin luettavan.</p>
+            <h2>
+                Lyhyesti minusta
+            </h2>
+            <p>
+                Nimeni on Jari Loippo. Olen toisen vuoden tieto- ja viestintätekniikan opiskelija. Valitsin ohjelmistotekniikan suuntautumisen, koska haluan kokea ohjelmointia mahdollisimman laajalti.
+            </p>
+            <p>
+                Harrastukset sivulta löytyy tietoa harrastuksista ja ei töihin liittyvistä asioista. Projektit sivulta pääsee ihailemaan koulutehtäviä.
+            </p>
+            <h2>
+                Taivoitteet
+            </h2>
+            <p>
+                Tavoitteeni on päästä kokeilemaan taitoja tosielämän tilanteissa. Arvostaisin jos pääsisin tekemään töitä ryhmässä. Uskon että oppisin tällöin tehokkaammin, kun pystyisin vertailemaan itseäni muiden töihin. 
+            </p>
+            <h2>
+                Osaamisalueet
+            </h2>
+            <h3>
+                Taidot
+            </h3>
+            <ul>
+                <li>Tietokoneiden kasaus ja ylläpito</li>
+                <li>Windows, linux ja Mac käyttöjärjestelmien hallinta</li>
+                <li>Ohjelmointi seuraavilla kielillä: C, C++, C#, JavaScript, HTML, CSS, PHP, (my)SQL</li>
+                <li>jQuery ja ajax on myös hallinnassa</li>
+                <li>Google API:en kanssa on tullut myös työkenneltyä</li>
+                <li>Front ja Back endien ohjelmointi. Esimerkki: projektit -> ttms0500 -> tuo</li>
+                <li>Arduino pohjaisten mikro-ohjain/elektroniikkalaitteiden ohjelmointi</li>
+            </ul>
+            <h2>
+                Kielitaito
+            </h2>
+            <ul>
+                <li>Suomi: äidinkieli</li>
+                <li>Englanti: erinomainen</li>
+                <li>Ruotsi: alkeet</li>
+                <li>Espania: alkeet</li>
+            </ul>
+            <h2>
+                Historia
+            </h2>
+            <h3>
+                Ammattikorkeakoulu (JAMK), 10.08.2015-10.05.2019
+            </h3>
+            <ul>
+                <li>Insinööriopiskelija, tieto- ja viestintätekniikan koulutusohjelma</li>
+                <li>Suuntautumien: Ohjelmistotekniikka</li>
+                <li>Suoritettujen opintopisteiden määrä 23 op (240op)</li>
+            </ul>
+            <h3>
+                Varusmiespalvelus, 2013
+            </h3>
+            <ul>
+                <li>Keuruun Pioneerirykmentti</li>
+                <li>Pioneeri</li>
+            </ul>
+        </section>
+        <footer><p>Mandatory footer.</p></footer>
+    </body>
+</html>
