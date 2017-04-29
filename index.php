0cc5375df41c685ca4f2e979008b43502fefe47f
K1729 <k1729@student.jamk.fi>
2017-04-29 03:26:07 +0300

    uli

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                 <a href="projects/">Projektit</a>
 		<a href="https://github.com/K1729">Github</a>
 		<a href="https://gitlab.com/K1729">Gitlab</a>
+		<a href="https://www.linkedin.com/in/jari-loippo-272331115/">LinkedIn</a>
             </nav>
         </header>
         
