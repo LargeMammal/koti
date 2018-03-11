04b9f70deb064f58fbb2a50d269b2be73c57e2e6
LargeMammal <large_mammal@outlook.com>
2018-03-11 23:36:38 +0200

    htaccess now works and started working on api

--- api.php
+++ api.php
@@ -1,6 +1,12 @@
 <?php
-    include 'php/checkFile.php';
-
-    $str = checkFile(index.php);
-    echo "Hello" . $str;
+    $arr = $_SERVER;
+    echo "Request method: " . $arr["REQUEST_METHOD"] . "<br>";
+    echo "Path info: " . $arr["PATH_INFO"] . "<br>";
+    echo "Request uri: " . $arr["REQUEST_URI"] . "<br>";
+    echo "Server signature: " . $arr["SERVER_SIGNATURE"] . "<br>";
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        echo "Recorded GET request!" . "<br>";
+    } else {
+        echo "api.php: only GET method supported";
+    }
 ?>
