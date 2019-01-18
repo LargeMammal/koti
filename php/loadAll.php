d6aeca88b67f1892a2c26532eaa9dc17c920aa8d
Jari Loippo <large_mammal@outlook.com>
2019-01-18 13:14:47 +0200

    Small fixes

--- php/loadAll.php
+++ php/loadAll.php
@@ -4,9 +4,9 @@ include "php/head/title.php";
 include "php/body/header.php";
 include "php/body/body.php";
 include "php/body/footer.php";
-// Load the whole page 
+// Load the whole page
 function loadAll() {
-    $str .= "<!DOCTYPE html><html><head>";
+    $str = "<!DOCTYPE html><html><head>";
     $str .= loadTitle();
     $str .= "</head><body>";
     $str .= loadHeader();
