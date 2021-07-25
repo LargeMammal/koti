d43196d8e478c3b096a551385b7dea0401f5440c
Jari Loippo <jari@loipposet.fi>
2021-07-25 12:54:19 +0300

    post dump test

--- classes/Server.class.php
+++ classes/Server.class.php
@@ -131,10 +131,9 @@ class Server {
          */
         private function post(): string
         {
-                //echo var_dump($_POST);
+                echo var_dump($_POST);
                 if (empty($_POST)) return 'Empty request';
-                echo var_dump($_POST['token']);
-                if ($_POST[0]['token'] === NULL) return 'Missing token';
+                if ($_POST["token"] === NULL) return 'Missing token';
                 if ((count($this->server) < 2) || $this->server[0] !== 'title')
                     return 'Malformed request';
 
