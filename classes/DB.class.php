e5b1a4cb17a1f9d327cb18078d5141970deaee3a
Jari Loippo <jari@loipposet.fi>
2021-07-28 00:17:05 +0300

    almost done

--- classes/DB.class.php
+++ classes/DB.class.php
@@ -294,8 +294,8 @@ class DB {
 		// insert tags
 		$sql = "INSERT INTO tags (";
 		$sql .= "hash, tag) VALUES";
-        foreach (explode('+', $dbitem['tags']) as $tag) {
-            $sql .= ' ('.$dbitem['hash'].','.$tag.'),';
+        foreach (explode('+', $dbitem->tags) as $tag) {
+            $sql .= ' ("'.$dbitem->hash.'","'.$tag.'"),';
         }
         trim($sql, ',');
 		echo "tags success\n";
